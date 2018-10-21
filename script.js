65c8fae10d937b537ca639971ed01efdb98f7ef5
Bernina Gray <bernina.gray@icloud.com>
2018-10-20 21:46:42 -0600

    Adding ColorPickerII instructions

--- script.js
+++ script.js
@@ -14,15 +14,15 @@ function initColorPicker() {
 function setColorPickerEventListeners(colorBox, rgb) {
 	rgb.red.addEventListener('change', () => {
 		console.log("Red value: ", rgb.red.value);
-		setBGColorBox (colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
+		setBoxBGColor (colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
 	});
 	rgb.green.addEventListener('change', () => {
 		console.log("Green value: ", rgb.green.value);
-		setBgColorBox (colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
+		setBoxBGColor (colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
 	});
 	rgb.blue.addEventListener('change', () => {
 		console.log("blue value: ", rgb.blue.value);
-		setBGColorBox (colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
+		setBoxBGColor (colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
 	});
 }
 
