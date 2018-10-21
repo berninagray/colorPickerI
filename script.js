f9e76358c3fa1bd28ce6b295a2a191f814e5016c
Bernina Gray <bernina.gray@icloud.com>
2018-10-20 22:13:28 -0600

    Corrected errors

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 window.onload = function () {
 	initColorPicker();
 }
+
 function initColorPicker() {
-	let colorbox = document.getElementById("color-box");
+	let colorBox = document.getElementById("color-box");
 	let rgb = {
 		red: document.getElementById("red"),
 		green: document.getElementById("green"),
 		blue: document.getElementById("blue"),
 	};
-	let colorPickers = document.getElementsByClassName("picker");
 	setColorPickerEventListeners(colorBox, rgb, colorPickers);
 }
 
@@ -16,7 +16,7 @@ function setColorPickerEventListeners(colorBox, rgb, pickerElements) {
 
 	for (let i = 0; i < pickerElements.length; i++) {
 		pickerElements [i].addEventListener('change', () => {
-			setBoxBGColor (colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
+			setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
 		});
 	}
 	rgb.red.addEventListener('change', () => {
